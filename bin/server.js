f42c0a128db111eeccbe406e383239c70370ddf6
Heather Taylor <heather@heysparkbox.com>
2017-05-21 17:22:52 -0400

    feat: setting up production server?

--- bin/server.js
+++ bin/server.js
@@ -9,18 +9,27 @@ const app = express();
 
 if (process.env.NODE_ENV === 'production'){
   // start production server???
-  //app.set('port', (process.env.PORT || 5000));
+  // set the port of the application
+  // process.env.PORT lets the port be set by Heroku
+  const port = process.env.PORT || 8080;
 
-  //app.use(express.static(__dirname + '/public'));
+  // set the view engine to ejs
+  app.set('view engine', 'ejs');
 
-  app.get('/', function(request, response) {
-    //response.render('index.html');
-    res.send('Hello World');
+  // make epress look in the public directory for assets
+  app.use(express.static(__dirname + '/public'));
+
+  // set the home page route
+  app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the view folder (ack - i don't have)
+    res.render('index');
   });
 
-  // app.listen(app.get('port'), function() {
-  //   console.log('Node app is running on port', app.get('port'));
-  // });
+  app.listen(port, function() {
+    console.log("Our app is running on http://localhost:" + port);
+  });
+
 }
 else {
   // start development server to watch and update html and css files

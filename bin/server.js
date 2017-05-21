e1b6f92df6bf6687f96538292e7b49a890def333
Heather Taylor <heather@heysparkbox.com>
2017-05-21 18:48:06 -0400

    feat: working it...

--- bin/server.js
+++ bin/server.js
@@ -34,6 +34,7 @@ if (process.env.NODE_ENV === 'production'){
 }
 else {
   // start development server to watch and update html and css files
+  console.log("You have accessed the development server.");
   shell.exec("browser-sync start --server --files 'public/css/*.css, *.html'");
 
   // using onchange to watch Sass file for changes, then rebuild files -- move

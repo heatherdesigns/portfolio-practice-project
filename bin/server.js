3d199bc8c2a7a81bdb31adf0dfbfb6be5b0dd701
Heather Taylor <heather@heysparkbox.com>
2017-05-22 10:11:55 -0400

    feat: update package.json and server.js

--- bin/server.js
+++ bin/server.js
@@ -6,6 +6,7 @@ const browser = require('browser-sync');
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
+const path = require('path');
 
 if (process.env.NODE_ENV === 'production'){
   // start production server...................
@@ -14,21 +15,22 @@ if (process.env.NODE_ENV === 'production'){
   // process.env.PORT lets the port be set by Heroku
   const port = process.env.PORT || 5000;
 
-  // make epress look in the public directory for assets
-  app.use(express.static(__dirname + '/public'));
-
-  // set the view engine to ejs
-  //app.set('views', __dirname + '/views');
-  app.set('views', '/public');
-  app.engine('html', ejs.renderFile);
-  app.set('view engine', 'html');
-
-  //set the home page route
-  app.get('/', function(request, response) {
-    // ejs render automatically looks in the view folder
-    //response.render('index');  used when when referenceing index.js file
-    response.render('index.html');
-  });
+  // make express look in the public directory for assets
+  console.log('PWD', path.join(__dirname, '..', 'public'));
+  app.use(express.static(__dirname + '../public'));
+  //
+  // // set the view engine to ejs
+  // //app.set('views', __dirname + '/views');
+  // app.set('views', '/public');
+  // app.engine('html', ejs.renderFile);
+  // app.set('view engine', 'html');
+  //
+  // //set the home page route
+  // app.get('/', function(request, response) {
+  //   // ejs render automatically looks in the view folder
+  //   //response.render('index');  used when when referenceing index.js file
+  //   response.render('index.html');
+  // });
 
   app.listen(port, function() {
     console.log("Our app is running on http://localhost:" + port);

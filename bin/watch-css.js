62023d333e7c33ea5aa240c534f80e1d6dfaa495
Heather Taylor <heather@heysparkbox.com>
2017-05-22 10:43:48 -0400

    feat: working to resolve CSS not rendering

--- bin/watch-css.js
+++ bin/watch-css.js
@@ -8,9 +8,3 @@ const mon = require('nodemon');
 // this gives "exec: internal error" on the command line upon exiting
 // runs ok with npm run start:server
 shell.exec("nodemon -e scss -x 'npm run build3-css'");
-
-// using onchange to watch Sass file for changes, then rebuild files -- move
-// this is working but not with start:server!!!!
-// shell.exec("onchange 'scss/*.scss' -- npm run build3-css");
-
-// experiment using chokidar

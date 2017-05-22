7b30d6e814ddbde707d5754ee5882231c7a553d2
Heather Taylor <heather@heysparkbox.com>
2017-05-22 10:21:19 -0400

    chore: remove comments

--- bin/server.js
+++ bin/server.js
@@ -11,7 +11,7 @@ const path = require('path');
 if (process.env.NODE_ENV === 'production'){
   // start production server...................
   console.log("You have accessed the production server.");
-  // set the port of the application
+
   // process.env.PORT lets the port be set by Heroku
   const port = process.env.PORT || 5000;
 
@@ -19,20 +19,6 @@ if (process.env.NODE_ENV === 'production'){
   console.log('PWD', path.join(__dirname, '..', 'public'));
   //app.use(express.static(__dirname + '../public'));
   app.use(express.static(path.join(__dirname, '..', 'public')));
-
-  //
-  // // set the view engine to ejs
-  // //app.set('views', __dirname + '/views');
-  // app.set('views', '/public');
-  // app.engine('html', ejs.renderFile);
-  // app.set('view engine', 'html');
-  //
-  // //set the home page route
-  // app.get('/', function(request, response) {
-  //   // ejs render automatically looks in the view folder
-  //   //response.render('index');  used when when referenceing index.js file
-  //   response.render('index.html');
-  // });
 
   app.listen(port, function() {
     console.log("Our app is running on http://localhost:" + port);

e526d3dc443706d625206ab1cd09f0c481b0db2b
Heather Taylor <heather@heysparkbox.com>
2017-05-22 09:11:40 -0400

    chore: clean out old files

--- bin/build-css.js
+++
@@ -1,8 +0,0 @@
-// keeping this as a separate file because it works.
-
-'use strict';
-
-const sass = require('node-sass');
-const shell = require('shelljs');
-
-shell.exec('node-sass scss/main.scss public/css/main.css');
--- bin/server.js
+++ bin/server.js
@@ -39,7 +39,4 @@ else {
   // start development server to watch and update html and css files
   console.log("You have accessed the development server.");
   shell.exec("browser-sync start --server --files 'public/css/*.css, *.html'");
-
-  // using onchange to watch Sass file for changes, then rebuild files -- move
-  // shell.exec("onchange 'scss/*.scss' -- npm run build3-css");
 }
